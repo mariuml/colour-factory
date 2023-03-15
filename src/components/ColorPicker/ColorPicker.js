0882175e2b423223414c2b9045cfbbbae7e5c5fd
MarKubil <marius.kub@outlook.com>
2023-03-15 19:42:10 +0000

    Created color picker

---
+++ src/components/ColorPicker/ColorPicker.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import Colorful from '@uiw/react-color-colorful';
+
+
+function ColorPicker() {
+    const [hex, setHex] = useState("#59c09a");
+
+    return (
+      <div style={{"width": "500px", margin: "auto", border: "3px solid black", background: hex}}>
+        <Colorful
+            style={{margin: "auto"}}
+          color={hex}
+          onChange={(color) => {
+            setHex(color.hexa);
+          }}
+        />
+        <div style={{ marginTop: 30, padding: 10 }}>
+          {hex}
+        </div>
+      </div>
+    );
+};
+
+export default ColorPicker;
