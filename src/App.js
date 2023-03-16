9491bc4ad53e6ef54d40b172607eb047d3475a82
YaszMoon <yasminite@gmail.com>
2023-03-16 19:22:06 +0000

    Check that mood button renders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ColorPicker from "./components/ColorPicker/ColorPicker";
+import MoodButton from "./components/Buttons/MoodButtons";
 
 function App() {
   return (
     <div className="App">
      <h1> The Colour Factory </h1>
      <ColorPicker />
+
+    <MoodButton />
     </div>
   );
 }
