5bf95da20282b3e119a6754e86599d78533122c7
mariuml <mariumladha97@gmail.com>
2023-03-16 21:12:11 +0000

    Reorganised page file structure and fixed duplicate colour pickers

--- src/components/ResultPage.js
+++ src/components/ResultPage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import GetColour from "./GetColour";
 
 export default function ResultPage() {
     return (
         <div>
             <h1>Result</h1>
+            <GetColour/>
         </div>
     )
 }
