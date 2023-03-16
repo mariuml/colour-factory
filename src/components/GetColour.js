08247e6c95f017aaba22b5ff5e9debca3bab222e
mariuml <mariumladha97@gmail.com>
2023-03-16 19:17:00 +0000

    Colour palette API response rendering to the page

--- src/components/GetColour.js
+++ src/components/GetColour.js
@@ -1,34 +1,41 @@
-import $ from 'jquery';
+import $ from "jquery";
 
+// Function to get an API response from the Colours API
 function GetColour() {
-    var hexCode = "0047";
-    var mode = "analogic";
-    var numberOfColours = "6";
+  var hexCode = "FF0000";
+  var mode = "analogic";
+  var numberOfColours = "5";
 
-    var queryURL = 
-    "https://www.thecolorapi.com/scheme?hex=" + hexCode +
-    "&mode" + mode + "&count" + numberOfColours;
-    
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-        async: false,
-      }).then(function (response) {
-    
-        console.log(response)
-    })
-    };
-    
+  var queryURL =
+    "https://www.thecolorapi.com/scheme?hex=" +
+    hexCode +
+    "&mode=" +
+    mode +
+    "&count=" +
+    numberOfColours;
 
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
 
-export default GetColour
-
-
-
+    // Saving the named palette image to myPalette variable
+    // var myPaletteUrl = response.image.named;
+    // var paletteDisplay = $("<img id='palette-display'>");
+    // paletteDisplay.attr("src", myPaletteUrl);
 
 
 
+    $( "#paletteDisplay" ).attr( "src", response.image.named);
+  });
 
+  return (
+    <div>
+      <h1> Here is your colour palette!</h1>
+      <img src="" id="paletteDisplay"></img>
+    </div>
+  );
+}
 
-
-
+export default GetColour;
