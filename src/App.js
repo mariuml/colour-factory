2a70f3d900239ed4ee6c4745a0936157680875ff
YaszMoon <yasminite@gmail.com>
2023-03-16 20:04:30 +0000

    Render some buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ColorPicker from "./components/ColorPicker/ColorPicker";
 import MoodButton from "./components/Buttons/MoodButtons";
+import moods from "./mood-color-data.json"
 
 function App() {
   return (
@@ -8,7 +9,12 @@ function App() {
      <h1> The Colour Factory </h1>
      <ColorPicker />
 
-    <MoodButton />
+{moods.map((m) => {
+  return (
+    <MoodButton name={m.name} color={m.color}/>
+  )
+})}
+    
     </div>
   );
 }
