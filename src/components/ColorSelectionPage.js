f3ad38b3627d34beee8bd670892fd5e34b10e6d9
YaszMoon <yasminite@gmail.com>
2023-03-17 18:39:50 +0000

    Add mood buttons

--- src/components/ColorSelectionPage.js
+++ src/components/ColorSelectionPage.js
@@ -1,31 +1,22 @@
-import React from "react"
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 import ColorPicker from "./ColorPicker";
 
-
-
 export default function SecondPage() {
-    return (
-        <div className="container-fluid row">
-            <div className="col-6">
-                <h3>
-                Select a mood:
-                </h3>
-                <div><p>mood</p></div>
-                <div><p>mood</p></div>
-                <div><p>mood</p></div>
-                <div><p>mood</p></div>
-            </div>
-            <div className="col-6">
-                <h1 className="text-center"> The Colour Factory </h1>
-                <ColorPicker />
-            </div>
+  return (
+    <div className="container-fluid row">
+      <div className="col-6">
+        <h3>Select a mood:</h3>
+        {moods.map((m) => {
+          return <MoodButton name={m.mood} color={m.color} />;
+        })}
+      </div>
+      <div className="col-6">
+        <h1 className="text-center"> The Colour Factory </h1>
+        <ColorPicker />
+      </div>
 
-            <NavLink
-                to="./ResultPage"
-            >
-                Next&gt;&gt;&gt;
-            </NavLink>
-        </div>
-    )
+      <NavLink to="./ResultPage">Next&gt;&gt;&gt;</NavLink>
+    </div>
+  );
 }
