e6411bb2e0a84bb152955d31f97ac340c748f691
YaszMoon <yasminite@gmail.com>
2023-03-17 19:16:52 +0000

    Create and update buttons

---
+++ src/components/Buttons/NextButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+function NextButton() {
+return (
+<Button variant="text">Next&gt;&gt;&gt;</Button>
+)}
+
+export default NextButton
---
+++ src/components/Buttons/SaveButton.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+function SaveButton() {
+    return (
+        <Button variant="contained">Save Palette</Button>
+    )
+}
+
+export default SaveButton
