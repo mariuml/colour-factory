044fdccc5373bc359dc6395c8c343336800d8261
YaszMoon <yasminite@gmail.com>
2023-03-21 19:30:51 +0000

    Update Error Alert

--- src/components/ErrorAlert.js
+++ src/components/ErrorAlert.js
@@ -1,34 +1,13 @@
 import React from "react";
+import { Alert, AlertTitle } from "@mui/material/";
 
 function ErrorAlert(props) {
   return (
-    <div
-      class="modal fade"
-      id="exampleModal"
-      tabindex="-1"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-5" id="exampleModalLabel">
-              Missing Information
-            </h1>
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div class="modal-body">
-            <p>{props.message}</p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Alert severity="error" style={{ width: "65%" }} className="m-auto">
+      <AlertTitle>Error</AlertTitle>
+      {props.message}
+    </Alert>
   );
 }
 
-export default ErrorAlert
+export default ErrorAlert;
