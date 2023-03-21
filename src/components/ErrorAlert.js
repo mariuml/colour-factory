7fcacad5409de874c53f2c3e2cdf1f23a09e658a
YaszMoon <yasminite@gmail.com>
2023-03-21 19:37:48 +0000

    Add some space above+below alert

--- src/components/ErrorAlert.js
+++ src/components/ErrorAlert.js
@@ -3,7 +3,11 @@ import { Alert, AlertTitle } from "@mui/material/";
 
 function ErrorAlert(props) {
   return (
-    <Alert severity="error" style={{ width: "65%" }} className="m-auto">
+    <Alert
+      severity="error"
+      style={{ width: "65%" }}
+      className="m-auto mt-5 mb-5"
+    >
       <AlertTitle>Error</AlertTitle>
       {props.message}
     </Alert>
